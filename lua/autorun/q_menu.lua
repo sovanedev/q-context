8afc7f4b9d6d910c9b2722b1b56bab3c54ee6d05
sovanedev <88293854+sovanedev@users.noreply.github.com>
2021-08-02 02:09:48 +0300

    Create q_menu.lua

---
+++ lua/autorun/q_menu.lua
@@ -0,0 +1,15 @@
+
+
+
+local qmenu_ranks = {
+["founder"] = true,
+["superadmin"] = true, 
+["admin"] = false
+}
+
+hook.Add("SpawnMenuOpen", "spawn_menu_ranks", function()
+local ply = LocalPlayer()
+  if qmenu_ranks[ply:GetUserGroup()] then
+    return true else return false
+  end
+end)
