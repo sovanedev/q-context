a75dd99a9b8fd2714184a5057b64c26a421c2fa4
sovanedev <88293854+sovanedev@users.noreply.github.com>
2023-02-22 20:38:58 +0400

    Update c_menu.lua

--- lua/autorun/c_menu.lua
+++ lua/autorun/c_menu.lua
@@ -2,15 +2,12 @@
 https://github.com/sovanedev
 =================================*\
 
-local qmenu_ranks = {
-["founder"] = true,
-["superadmin"] = true, 
-["admin"] = false
+local cmenu_ranks = {
+  ["founder"] = true,
+  ["superadmin"] = true, 
+  ["admin"] = false
 }
 
-hook.Add("ContextMenuOpen", "spawn_menu_ranks", function()
-local ply = LocalPlayer()
-  if qmenu_ranks[ply:GetUserGroup()] then
-    return true else return false
-  end
+hook.Add("ContextMenuOpen", "spawn_menu_ranks", function(ply)
+  return cmenu_ranks[ply:GetUserGroup()]
 end)
